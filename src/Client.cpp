f59fe47ca619ba434002be139ae040cb42082e76
m010 <mi243@ya.ru>
2021-03-05 18:23:45 +0300

    for ttamesha

--- src/Client.cpp
+++ src/Client.cpp
@@ -4,7 +4,6 @@
 std::string generate_response(const Request& request, const ServConfig &config) {
 
 	std::cout << "tossern" << std::endl;
-	// for test
 	char msg[] = "hello from server\n";
 
 	std::stringstream response_body;
@@ -15,7 +14,6 @@ std::string generate_response(const Request& request, const ServConfig &config) 
 				  << "<pre>" << msg << "</pre>\n"
 				  << "<em><small>Test C++ Http Server</small></em>\n";
 
-	// Формируем весь ответ вместе с заголовками
 	std::stringstream response;
 	response << "HTTP/1.1 200 OK\r\n"
 			 << "Version: HTTP/1.1\r\n"
@@ -23,10 +21,7 @@ std::string generate_response(const Request& request, const ServConfig &config) 
 			 << "Content-Length: " << response_body.str().length()
 			 << "\r\n\r\n"
 			 << response_body.str();
-	// for test
 
-	//if (ret == -1)
-	//	throw Error("send message");
 	return response.str();
 }
 
@@ -34,10 +29,8 @@ bool Client::response() {
 	if (status_ == READY_TO_READ)
 		return false;
 	std::string response = generate_response(req_, serv_);
-	write(fd_ ,response.c_str(), response.size());
-
-	//TODO: Do we need it? bzero(this->_buffer, 2048);
-	int ret = send(fd_, response.c_str(), response.length(), 0);
+	int ret = send(fd_, response.c_str(), response.length(), 0); //TODO: CHECK RET
+	req_.clear();
 	return status_ == CLOSE_CONNECTION;
 }
 
@@ -53,24 +46,26 @@ void Client::receive() {
 	if (status_ != READY_TO_READ)
 		return;
 	try {
-		int ret;
-		char buffer[2049];
-		std::string raw_request;
+		//int ret;
+		//char buffer[2049];
+		//std::string raw_request;
 		//TODO: MOVE TO REQUEST
-		while ((ret = read(fd_, buffer, 2048)) > 0) {
-			buffer[ret] = 0x0;
-			raw_request += buffer;
-		}
-		req_ = Request(raw_request);
+		//while ((ret = read(fd_, buffer, 2048)) > 0) {
+		//	buffer[ret] = 0x0;
+		//	raw_request += buffer;
+		//}
+		if (!req_.receive())
+			status_ = CLOSE_CONNECTION;
+		else
+			status_ = READY_TO_SEND;
+		//req_ = Request(raw_request);
 		std::cout << "<REQUEST\n" << req_ << std::endl;
 		std::cout << "REQUEST>\n"; //test
-		status_ = CLOSE_CONNECTION;
 	}
-	catch (...) {
+	catch (std::exception &) {
 		std::cout << "TODO: handle the half msg!!!" << std::endl;
 	}
 }
-
 Client::Client(const ServConfig &serv, int fd) : serv_(serv), fd_(fd), status_(READY_TO_READ), req_("")
 {}
 
