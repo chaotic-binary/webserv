eefab1f997d9b439b865a9f1e011791b205ab602
m010 <mi243@ya.ru>
2021-03-15 23:38:47 +0300

    Refactor includes

--- include/Client.h
+++ include/Client.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "ServConfig.h"
 #include "Request.h"
 
@@ -12,11 +13,16 @@ private:
 	const int fd_;
 	enum e_client_status status_;
 	Request req_;
-
 	__unused sockaddr_in				_clientAddr;
 public:
 	void receive();
 	bool response();
+	void raw_send(const std::string& msg){
+		send(fd_, msg.c_str(), msg.length(), 0);
+	}
+	e_client_status GetStatus() {
+		return status_;
+	}
 	const ServConfig &getServ() const;
 	int getFd() const;
 };
--- include/response.h
+++ include/response.h
@@ -10,19 +10,31 @@
 class Response {
 public:
 	typedef std::map<std::string, std::string> Headers;
-	explicit Response(int code);
+	explicit Response(int code = 500);
 	std::string Generate();
 	void SetHeader(const std::string &title, const std::string &content);
   	void SetHeader(const std::string &title, const std::vector<std::string>& multi_content);
-
 	void SetBody(const std::string& body);
 	std::string GetHeader(const std::string &title) const;
-
 private:
-	std::string GetCurDate();
+	std::string GetCurDate() const;
 
 	void generate_error_page(int code);
 	int code_;
-	Headers headers_;
+ public:
+  int GetCode() const;
+ private:
+  Headers headers_;
 	std::string body_;
+};
+
+class RespException : public std::exception
+{
+ public:
+  	virtual ~RespException()  throw();
+	const char * what() const throw();
+  explicit RespException(const Response &rsp) throw();
+  const Response &GetRsp() const;
+ private:
+  Response rsp_;
 };
