c944e74ba9bc47b6cc3943e17f4dca9dc2216092
MakhmudEgo <MakhmudEgo@yandex.ru>
2021-03-18 11:30:22 +0300

    test commit

---
+++ src/HeadMethod.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <method_utils.h>
+#include <mimeTypes.h>
+#include "GetMethod.h"
+#include "ServConfig.h"
+
+Response GetGenerator(const Request &request, const ServConfig &config) {
+	Response rsp(200);
+	const Location &location = config.getLocation(request.getReqTarget());
+	const std::string obj = checkSource(location, request.getReqTarget());
+
+	std::ifstream file(obj);
+	std::stringstream ss;
+	ss << file.rdbuf();
+	file.close();
+
+	rsp.SetBody(ss.str());
+	rsp.SetHeader("content-type", getMimeType(obj));
+	return rsp;
+
+}
