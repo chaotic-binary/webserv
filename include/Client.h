d50143121df517114cf5466176ecdcba13fc8374
m010 <mi243@ya.ru>
2021-03-23 16:06:15 +0300

    Added check time of request in Client and Added partial send [partial send not working?]

--- include/Client.h
+++ include/Client.h
@@ -2,6 +2,7 @@
 
 #include "ServConfig.h"
 #include "Request.h"
+#include <sys/time.h>
 
 class Client {
 public:
@@ -12,13 +13,26 @@ private:
 	const ServConfig &serv_;
 	const int fd_;
 	enum e_client_status status_;
+	struct timeval tv_;
 	Request req_;
 	__unused sockaddr_in				_clientAddr;
 public:
 	void receive();
 	bool response();
 	void raw_send(const std::string& msg){
-		send(fd_, msg.c_str(), msg.length(), 0);
+		size_t sended = 0;
+		while (sended < msg.size())
+			if(msg.size() - sended < 1024)
+				sended += send(fd_, &(msg.c_str()[sended]), msg.size() - sended,0); //TODO check send ret
+			else
+				sended += send(fd_, &(msg.c_str()[sended]), 1024,0); //TODO check send ret
+	}
+	void check()
+	{
+		struct timeval cur;
+		gettimeofday(&cur, NULL);
+		if(cur.tv_sec  - tv_.tv_sec > 120) //TODO: delete magic number
+			status_ = CLOSE_CONNECTION;
 	}
 	e_client_status GetStatus() {
 		return status_;
