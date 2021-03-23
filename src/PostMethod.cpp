1bbdc4a70415c396bf6df2a5f748aa19c3847e8d
m010 <mi243@ya.ru>
2021-03-23 16:08:49 +0300

    cgi whith modified env, checkSource and with kostil(erase) parsing [prob. working]

--- src/PostMethod.cpp
+++ src/PostMethod.cpp
@@ -6,18 +6,21 @@
 Response PostGenerator(const Request &request, const ServConfig &config) {
 	Response rsp(200);
 	const Location& location = config.getLocation(request.getReqTarget());
-	std::string path = checkSource(location, request.getReqTarget(), true);
-	const std::vector<std::string> &cgi_extentions = location.getCgiExtensions();
-	if (cgi_extentions.empty() || cgi_extentions.end() == find(cgi_extentions.begin(), cgi_extentions.end(), path.substr(path.rfind('.'))))
-		return Response(404);
-	try {
-		EnvironMap tmp = CgiGenerateEnv(request, config);
-		std::string total = CgiEx(location.getCgiPath(), path, request.getBody(), tmp);
-		rsp.SetBody(total);
-	} catch (...)
-	{
-		return Response(500);
-	}
+	//TODO: is this cgi Correct?!
+	//std::string path = checkSource(location, request.getReqTarget(), true);
+	//std::cerr << "!!!!"  << path ;
+	__unused	const std::vector<std::string> &cgi_extentions = location.getCgiExtensions();
+
+//	if (cgi_extentions.empty() ||
+//	cgi_extentions.end() == find(cgi_extentions.begin(), cgi_extentions.end(), path.substr(path.rfind('.'))))
+//		return Response(404);
+	std::string	path = request.getReqTarget();
+	path.erase(0, location.getPath().size() + 1);
+	path = location.getRoot() + path;
+
+	EnvironMap tmp = CgiGenerateEnv(request, config);
+	std::string total = CgiEx(location.getCgiPath(), path, request.getBody(), tmp);
+	rsp.SetBody(total);
 	//rsp.SetHeader("content-type", getMimeType(obj));
 	return rsp;
 }
