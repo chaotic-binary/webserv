9c1fdafd79934b6fdc34454c28f06639fa7a718e
MakhmudEgo <MakhmudEgo@yandex.ru>
2021-03-29 13:32:57 +0300

    added Content-Language, Last-Modified headers

--- src/GetMethod.cpp
+++ src/GetMethod.cpp
@@ -16,6 +16,23 @@ Response GetGenerator(const Request &request, const ServConfig &config) {
 	file.close();
 
 	rsp.SetBody(ss.str());
+
+	//tmp part
+	{
+		struct stat sb;
+		lstat(obj.c_str(), &sb);
+		char buff[1024];
+		tm date;
+		strptime(std::to_string(sb.st_mtimespec.tv_sec).c_str(), "%s", &date);
+		size_t ret = strftime(buff, 1024, "%a, %d %b %Y %T GMT", &date);
+		buff[ret] = '\0';
+		rsp.SetHeader("Last-Modified", buff);
+
+		size_t i;
+		i =  ss.str().find("<html lang=");
+		rsp.SetHeader("Content-Language", i != std::string::npos ? ss.str().substr(i + 12,  2) : "");
+	}
+
 	rsp.SetHeader("content-type", getMimeType(obj));
 	return rsp;
 
